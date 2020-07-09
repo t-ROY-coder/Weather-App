c1f3decf0fe3e73479a5162156d750403cc40a9c
TANUMON ROY <roytanumon@gmail.com>
2020-07-09 18:42:44 +0530

    Setup app for Heroku

--- public/js/clientApp.js
+++ public/js/clientApp.js
@@ -5,20 +5,20 @@ const search = document.querySelector("input");
 const msg1 = document.querySelector("#msg1");
 const msg2 = document.querySelector("#msg2");
 weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  msg1.textContent = "Loading...";
-  msg2.textContent = "";
-  fetch(
-    "http://localhost:3000/weather?address=" + encodeURIComponent(search.value)
-  ).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        msg1.textContent = "ERROR!";
-        msg2.textContent = data.error;
-      } else {
-        msg1.textContent = data.location;
-        msg2.textContent = data.forecast.description;
-      }
-    });
-  });
+    e.preventDefault();
+    msg1.textContent = "Loading...";
+    msg2.textContent = "";
+    fetch("/weather?address=" + encodeURIComponent(search.value)).then(
+        (response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    msg1.textContent = "ERROR!";
+                    msg2.textContent = data.error;
+                } else {
+                    msg1.textContent = data.location;
+                    msg2.textContent = data.forecast.description;
+                }
+            });
+        }
+    );
 });
