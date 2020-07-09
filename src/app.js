1d999acbc1178e02dbaa6d8b97a05f20f3991b40
TANUMON ROY <roytanumon@gmail.com>
2020-07-09 18:35:19 +0530

    UI update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const hbs = require("hbs");
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDir = path.join(__dirname, "../public");
@@ -19,69 +21,69 @@ hbs.registerPartials(partialsPath);
 app.use(express.static(path.join(publicDir)));
 
 app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather",
-    name: "Roy",
-  });
+    res.render("index", {
+        title: "Weather",
+        name: "T. Roy",
+    });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About",
-    name: "Roy",
-  });
+    res.render("about", {
+        title: "About",
+        name: "T. Roy",
+    });
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", {
-    title: "Help",
-    msg: "This is the help page",
-    name: "Roy",
-  });
+    res.render("help", {
+        title: "Help",
+        msg: "This is the help page",
+        name: "T. Roy",
+    });
 });
 
 app.get("/weather", (req, res) => {
-  if (!req.query.address) {
-    return res.send({
-      error: "Please provide an address",
-    });
-  }
-
-  geocode(
-    req.query.address,
-    (error, { lattitude, longitude, location } = {}) => {
-      if (error) {
+    if (!req.query.address) {
         return res.send({
-          error: error,
+            error: "Please provide an address",
         });
-      }
-      forecast(lattitude, longitude, (error, data) => {
-        if (error) {
-          return res.send({
-            error: error,
-          });
-        }
-        res.send({
-          location: location,
-          forecast: data,
-        });
-      });
     }
-  );
+
+    geocode(
+        req.query.address,
+        (error, { lattitude, longitude, location } = {}) => {
+            if (error) {
+                return res.send({
+                    error: error,
+                });
+            }
+            forecast(lattitude, longitude, (error, data) => {
+                if (error) {
+                    return res.send({
+                        error: error,
+                    });
+                }
+                res.send({
+                    location: location,
+                    forecast: data,
+                });
+            });
+        }
+    );
 });
 
 app.get("/help/*", (req, res) => {
-  res.render("404", {
-    msg: "Help article NOT found",
-  });
+    res.render("404", {
+        msg: "Help article NOT found",
+    });
 });
 
 app.get("*", (req, res) => {
-  res.render("404", {
-    msg: "404: Page NOT found",
-  });
+    res.render("404", {
+        msg: "404: Page NOT found",
+    });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
 });
