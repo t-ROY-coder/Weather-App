cd6954f54bc4596d33a79e4e16f6d2dbb0865ee9
TANUMON ROY <roytanumon@gmail.com>
2020-07-11 23:45:16 +0530

    UI update

--- public/js/clientApp.js
+++ public/js/clientApp.js
@@ -4,21 +4,34 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const msg1 = document.querySelector("#msg1");
 const msg2 = document.querySelector("#msg2");
+const msg3 = document.querySelector("#msg3");
+const msg4 = document.querySelector("#msg4");
+const msg5 = document.querySelector("#msg5");
+
 weatherForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    msg1.textContent = "Loading...";
-    msg2.textContent = "";
-    fetch("/weather?address=" + encodeURIComponent(search.value)).then(
-        (response) => {
-            response.json().then((data) => {
-                if (data.error) {
-                    msg1.textContent = "ERROR!";
-                    msg2.textContent = data.error;
-                } else {
-                    msg1.textContent = data.location;
-                    msg2.textContent = data.forecast.description;
-                }
-            });
-        }
-    );
+   e.preventDefault();
+   msg1.textContent = "Loading...";
+   msg2.textContent = "";
+   fetch("/weather?address=" + encodeURIComponent(search.value)).then(
+      (response) => {
+         response.json().then((data) => {
+            if (data.error) {
+               msg1.textContent = "ERROR!";
+               msg2.textContent = data.error;
+            } else {
+               msg1.textContent = "Location: " + data.location;
+               msg2.textContent = "Forecast: " + data.forecast.description;
+               msg3.textContent =
+                  "Temperature: " +
+                  data.forecast.temperature +
+                  " degree Celsius";
+               msg4.textContent =
+                  "Apparent Temperature: " +
+                  data.forecast.app_temperature +
+                  " degree Celsius";
+               msg5.textContent = "Humidity: " + data.forecast.humidity + "%";
+            }
+         });
+      }
+   );
 });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,26 @@
 const request = require("postman-request");
 
 const forcast = (lat, long, callback) => {
-  const url =
-    "http://api.weatherstack.com/current?access_key=82311ca9440e51fadafee6142c30012e&query=" +
-    encodeURIComponent(lat) +
-    "," +
-    encodeURIComponent(long);
-  request({ url, json: true }, (error, { body }) => {
-    if (error) {
-      callback("Unable to connect to weather services!", undefined);
-    } else if (body.error) {
-      callback("Unable to find location. Try another search.", undefined);
-    } else {
-      const currData = body.current;
-      callback(undefined, {
-        description: currData.weather_descriptions[0],
-        temperature: currData.temperature,
-        app_temperature: currData.feelslike,
-      });
-    }
-  });
+   const url =
+      "http://api.weatherstack.com/current?access_key=82311ca9440e51fadafee6142c30012e&query=" +
+      encodeURIComponent(lat) +
+      "," +
+      encodeURIComponent(long);
+   request({ url, json: true }, (error, { body }) => {
+      if (error) {
+         callback("Unable to connect to weather services!", undefined);
+      } else if (body.error) {
+         callback("Unable to find location. Try another search.", undefined);
+      } else {
+         const currData = body.current;
+         callback(undefined, {
+            description: currData.weather_descriptions[0],
+            temperature: currData.temperature,
+            app_temperature: currData.feelslike,
+            humidity: currData.humidity,
+         });
+      }
+   });
 };
 
 module.exports = forcast;
